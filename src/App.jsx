1b8349f5bf2962f7f992e3ccc28da8440cde2aa1
krnlpawar <krnlpawar@gmail.com>
2025-01-07 22:28:59 +0530

    added team section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Menu from './components/Menu'
 import Services from './components/Services'
+import Team from './components/Team'
 
 function App() {
   return (
     <>
       <Menu/>
       <Services/>
+      <Team/>
     </>
   )
 }
